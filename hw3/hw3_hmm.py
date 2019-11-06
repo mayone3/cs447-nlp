e8ce15e8946a565dcb56f72d5717cdb24c773c58
Siwei Xian <sxian3@linux-07.ews.illinois.edu>
2019-11-06 12:35:17 -0600

    cs447

---
+++ hw3/hw3_hmm.py
@@ -0,0 +1,123 @@
+########################################
+## CS447 Natural Language Processing  ##
+##           Homework 2               ##
+##       Julia Hockenmaier            ##
+##       juliahmr@illnois.edu         ##
+########################################
+##
+## Part 1:
+## Train a bigram HMM for POS tagging
+##
+import os.path
+import sys
+from operator import itemgetter
+from collections import defaultdict
+from math import log
+
+# Unknown word token
+UNK = 'UNK'
+
+# Class that stores a word and tag together
+class TaggedWord:
+    def __init__(self, taggedString):
+        parts = taggedString.split('_');
+        self.word = parts[0]
+        self.tag = parts[1]
+
+# Class definition for a bigram HMM
+class HMM:
+### Helper file I/O methods ###
+    ################################
+    #intput:                       #
+    #    inputFile: string         #
+    #output: list                  #
+    ################################
+    # Reads a labeled data inputFile, and returns a nested list of sentences, where each sentence is a list of TaggedWord objects
+    def readLabeledData(self, inputFile):
+        if os.path.isfile(inputFile):
+            file = open(inputFile, "r") # open the input file in read-only mode
+            sens = [];
+            for line in file:
+                raw = line.split()
+                sentence = []
+                for token in raw:
+                    sentence.append(TaggedWord(token))
+                sens.append(sentence) # append this list as an element to the list of sentences
+            return sens
+        else:
+            print("Error: unlabeled data file %s does not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
+            sys.exit() # exit the script
+
+    ################################
+    #intput:                       #
+    #    inputFile: string         #
+    #output: list                  #
+    ################################
+    # Reads an unlabeled data inputFile, and returns a nested list of sentences, where each sentence is a list of strings
+    def readUnlabeledData(self, inputFile):
+        if os.path.isfile(inputFile):
+            file = open(inputFile, "r") # open the input file in read-only mode
+            sens = [];
+            for line in file:
+                sentence = line.split() # split the line into a list of words
+                sens.append(sentence) # append this list as an element to the list of sentences
+            return sens
+        else:
+            print("Error: unlabeled data file %s ddoes not exist" % inputFile)  # We should really be throwing an exception here, but for simplicity's sake, this will suffice.
+            sys.exit() # exit the script
+### End file I/O methods ###
+
+    ################################
+    #intput:                       #
+    #    unknownWordThreshold: int #
+    #output: None                  #
+    ################################
+    # Constructor
+    def __init__(self, unknownWordThreshold=5):
+        # Unknown word threshold, default value is 5 (words occuring fewer than 5 times should be treated as UNK)
+        self.minFreq = unknownWordThreshold
+        ### Initialize the rest of your data structures here ###
+
+    ################################
+    #intput:                       #
+    #    trainFile: string         #
+    #output: None                  #
+    ################################
+    # Given labeled corpus in trainFile, build the HMM distributions from the observed counts
+    def train(self, trainFile):
+        data = self.readLabeledData(trainFile) # data is a nested list of TaggedWords
+        #print("Your first task is to train a bigram HMM tagger from an input file of POS-tagged text")
+
+    ################################
+    #intput:                       #
+    #     testFile: string         #
+    #    outFile: string           #
+    #output: None                  #
+    ################################
+    # Given an unlabeled corpus in testFile, output the Viterbi tag sequences as a labeled corpus in outFile
+    def test(self, testFile, outFile):
+        data = self.readUnlabeledData(testFile)
+        f=open(outFile, 'w+')
+        for sen in data:
+            vitTags = self.viterbi(sen)
+            senString = ''
+            for i in range(len(sen)):
+                senString += sen[i]+"_"+vitTags[i]+" "
+            print(senString.rstrip(), end="\n", file=f)
+
+    ################################
+    #intput:                       #
+    #    words: list               #
+    #output: list                  #
+    ################################
+    # Given a list of words, runs the Viterbi algorithm and returns a list containing the sequence of tags
+    # that generates the word sequence with highest probability, according to this HMM
+    def viterbi(self, words):
+        #print("Your second task is to implement the Viterbi algorithm for the HMM tagger")
+        # returns the list of Viterbi POS tags (strings)
+        return ["NULL"]*len(words) # this returns a dummy list of "NULL", equal in length to words
+
+if __name__ == "__main__":
+    tagger = HMM()
+    tagger.train('train.txt')
+    tagger.test('test.txt', 'out.txt')
